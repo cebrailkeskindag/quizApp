7cd617415edc744ecd01de6501ca9621a3b17475
cebrailkeskindag <cebrailkeskindag1549@gmail.com>
2023-10-31 13:04:12 +0300

    Resim ve bilgiler eklendi.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text("Hello World")));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor:
+            Color.fromARGB(255, 10, 105, 163), //Configuration Widgets
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/profil.jpeg',
+                height: 200,
+                width: 200,
+              ),
+              Text(
+                "Cebrail Keskindağ",
+                style: TextStyle(fontSize: 35, color: Colors.white),
+              ),
+              Text(
+                "Mobil Uygulama Flutter-1A",
+                style: TextStyle(fontSize: 30, color: Colors.white),
+              ),
+              Text(
+                "31/10/2023",
+                style: TextStyle(fontSize: 35, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
 }
-
+//İntellisense=>ctrl + space
+//const
+//shift + alt + f
