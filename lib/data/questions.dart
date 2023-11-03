ef7fb6b355bdfe33a9cf9c9818db84ec9f18930e
cebrailkeskindag <cebrailkeskindag1549@gmail.com>
2023-11-03 11:00:51 +0300

    butona bastıkça yeni soruya geçmesi

---
+++ lib/data/questions.dart
@@ -0,0 +1,12 @@
+import 'package:tasarim_calismasi/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion("Aşağıdakilerden hangisi flutterda state'i update eder?",
+      ["setState", "constructor", "class", "statelesswidget"]),
+  QuizQuestion("Aşağıdakilerden hangisi flutterda sınıfı temsil eder?",
+      ["setState", "constructor", "class", "statelesswidget"]),
+  QuizQuestion("Aşağıdakilerden hangisi flutterda state'i statiktir?",
+      ["setState", "constructor", "class", "statelesswidget"]),
+  QuizQuestion("Aşağıdakilerden hangisi flutterda değiştirilemz?",
+      ["setState", "constructor", "class", "statelesswidget"])
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:tasarim_calismasi/data/questions.dart';
+import 'package:tasarim_calismasi/models/quiz_question.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor:
-            Color.fromARGB(255, 10, 105, 163), //Configuration Widgets
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/profil.jpeg',
-                height: 200,
-                width: 200,
-              ),
-              Text(
-                "Cebrail Keskindağ",
-                style: TextStyle(fontSize: 35, color: Colors.white),
-              ),
-              Text(
-                "Mobil Uygulama Flutter-1A",
-                style: TextStyle(fontSize: 30, color: Colors.white),
-              ),
-              Text(
-                "31/10/2023",
-                style: TextStyle(fontSize: 35, color: Colors.white),
-              ),
-            ],
-          ),
+  runApp(const MaterialApp(home: QuestionScreen()));
+}
+
+// Stateless => Ekranda değişime uğramayacak, UI widget
+// CTRL + .
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurpleAccent,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/profil.jpeg", width: 250),
+            const Text(
+              "Quiz App",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold),
+            ),
+            OutlinedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.arrow_right_alt),
+              label: const Text("Start"),
+              style: OutlinedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  padding: const EdgeInsets.fromLTRB(40, 20, 40, 20)),
+            ),
+          ],
         ),
       ),
-    ),
-  );
+    );
+  }
 }
-//İntellisense=>ctrl + space
-//const
-//shift + alt + f
+
+// Boilerplate
+
+class QuestionScreen extends StatefulWidget {
+  const QuestionScreen({Key? key}) : super(key: key);
+
+  @override
+  _QuestionState createState() => _QuestionState();
+}
+
+class _QuestionState extends State<QuestionScreen> {
+  var sayac = 0;
+
+  void changeQuestion() {
+    setState(() {
+      sayac += 1;
+    }); // değişikliklerin ekrana da yansıtılması için gerekli..
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("ÖDEV"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(questions[sayac].question),
+            ...questions[sayac].answers.map((answer) {
+              return ElevatedButton(
+                onPressed: () {
+                  if (sayac < 3) {
+                    changeQuestion();
+                  }
+                },
+                child: Container(
+                  child: Text(answer),
+                  width: 100,
+                  height: 30,
+                ),
+              );
+            })
+          ],
+        ),
+      ),
+    );
+  }
+}
